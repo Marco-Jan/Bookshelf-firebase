c6698de29962e2aadf79d668a11a4e1a0ca3467b
Baloou <127010557+Marco-Jan@users.noreply.github.com>
2024-02-26 13:04:52 +0100

    first data

---
+++ src/firebase/firebaseInit.ts
@@ -0,0 +1,40 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { collection, getDocs } from 'firebase/firestore';
+
+
+
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCAo--NfO4djLGdwD0LYKut_Vf-XeJGs6I",
+    authDomain: "bookshelf-2-0-92549.firebaseapp.com",
+    projectId: "bookshelf-2-0-92549",
+    storageBucket: "bookshelf-2-0-92549.appspot.com",
+    messagingSenderId: "814189133391",
+    appId: "1:814189133391:web:7af39d3c20835a85ef743a"
+};
+
+
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+
+const querySnapshot = await getDocs(collection(db, 'books'));
+querySnapshot.forEach((doc) => {
+    //doc.data()
+    console.log(doc.id, '=>', doc.data);
+
+
+});
+
+
+ 
---
+++ src/firebase/useBooks.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { db } from './firebaseInit'
+import { collection, onSnapshot,query, orderBy } from "firebase/firestore";
+
+
+
+
+type TBook = {
+    id: string;
+    title: string;
+    author: string;
+    pages: number;
+    read: boolean;
+    createdAt: Date;
+    imageURL: string;
+    uid: string;
+};
+
+
+export default function useBooks() {
+
+    const [books, setBooks] = useState<TBook[]>([]);
+    useEffect(() => {
+        const q = query(collection(db, 'books'), orderBy('createdAt', 'desc'));
+        onSnapshot(q, (snapshot) => {
+            setBooks(
+                snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id } as TBook))
+            );
+        });
+    }, []);
+    return [books]
+}
